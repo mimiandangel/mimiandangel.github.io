29c97e33e3e1de0f971926c11b339029e130ccfd
mimiandangel <rahernandezav@est.utn.ac.cr>
2024-11-14 13:11:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Puedes agregar funcionalidades adicionales en JavaScript aquí
+document.querySelectorAll('.interactive-photo').forEach(photo => {
+    photo.addEventListener('click', () => {
+        alert("¡Momentos inolvidables juntos!");
+    });
+});
